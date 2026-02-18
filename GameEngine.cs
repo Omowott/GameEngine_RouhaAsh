62bd19359532f034d1cbf8e0aeb90aa46ac03fe1
Ash Rouha <ashrouha1@gmail.com>
2026-02-18 21:21:45 +0100

    2.2 Consistency et Real Time
    je pense c'est correct jusqu'ici

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,56 @@ namespace GameEngine_RouhaAsh
 {
     internal class GameEngine
     {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        private bool _shouldQuit;
+
         public void Run()
         {
-            ProcessInput();
-            Update();
-            Render();
+            _stopwatch.Start();
+
+            const float FIXED_FRAME_TIME = 20 / 1000.0f;
+            float lag = 0.0f;
+            float last_time = GetCurrentTime();
+           
+            while (!_shouldQuit)
+            {
+                float loop_start_time = GetCurrentTime();
+                float elapsed_time = loop_start_time - last_time;
+                lag += elapsed_time;
+                ProcessInput();
+
+                while (lag >= FIXED_FRAME_TIME)
+                {
+                    Update(FIXED_FRAME_TIME);
+                    lag -= FIXED_FRAME_TIME;
+                }
+
+                Render();
+                last_time = loop_start_time;
+            }
+
+            
         }
         //------------------------------------------------
         private void ProcessInput()
         {
+            ConsoleKeyInfo player_command = Console.ReadKey(true);
+            if(player_command.Key == ConsoleKey.Enter)
+            {
 
+            }
         }
-        private void Update()
+        private void Update(float elapsed_time)
         {
 
         }
         private void Render()
         {
 
+        }
+        private float GetCurrentTime()
+        {
+            return _stopwatch.ElapsedMilliseconds / 1000.0f;
         }
     }
 }
